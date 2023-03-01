0d3b11cfeae7da4314d0e30ed3f52075c0bed102
eugene.k <mcxemic@gmail.com>
2023-03-01 14:55:57 +0100

    fix

--- user_update.py
+++ user_update.py
@@ -10,27 +10,34 @@ def procces_if_update(message, student: Student):
         pass
     elif (message.text == constants.YES):
         markup = build_reply_markup(("Далі"))
-        msg = bot.send_message(message.chat.id,
-                               """Ми пройдемось по кожному з параметрів та змінимо їх за необхідності.\
+        msg = bot.send_message(
+            message.chat.id,
+            """Ми пройдемось по кожному з параметрів та змінимо їх за необхідності.\
                 Натиснііть кнопку щоб почати :)""",
-                               reply_markup=markup)
+            reply_markup=markup
+        )
         bot.register_next_step_handler(msg, ask_change_mail, student)
 
 
 def ask_change_mail(message, student: Student):
     markup = build_reply_markup(constants.YES_NO)
-    msg = bot.send_message(message.chat.id,
-                           f"Ваша пошта {student.mail}. Треба її змінити?",
-                           reply_markup=markup)
+    msg = bot.send_message(
+        message.chat.id,
+        f"Ваша пошта {student.mail}. Треба її змінити?",
+        reply_markup=markup
+    )
     bot.register_next_step_handler(msg, procces_if_change_mail, student)
 
 
 def procces_if_change_mail(message, student: Student):
     if message.text == constants.YES:
-        msg = bot.send_message(message.chat.id,
-                               f"Введіть нову електронну пошту:")
+        msg = bot.send_message(
+            message.chat.id,
+            f"Введіть нову електронну пошту:"
+        )
         bot.register_next_step_handler(
-            msg, procces_change_mail, student=student)
+            msg, procces_change_mail, student=student
+        )
     elif message.text == constants.NO:
         ask_change_live_place(message, student)
 
@@ -42,9 +49,11 @@ def procces_change_mail(message, student: Student):
 
 def ask_change_live_place(message, student: Student):
     markup = build_reply_markup(constants.YES_NO)
-    msg = bot.send_message(message.chat.id,
-                           f"Ви живете в {student.live_place}. Треба її змінити?",
-                           reply_markup=markup)
+    msg = bot.send_message(
+        message.chat.id,
+        f"Ви живете в {student.live_place}. Треба її змінити?",
+        reply_markup=markup
+    )
     bot.register_next_step_handler(msg, procces_if_change_live_place, student)
 
 
@@ -65,18 +74,23 @@ def procces_change_live_place(message, student: Student):
 
 def ask_change_work(message, student: Student):
     markup = build_reply_markup(constants.YES_NO)
-    msg = bot.send_message(message.chat.id,
-                           f"Ваша робота \"{student.work}\". Треба змінити інформацію?",
-                           reply_markup=markup)
+    msg = bot.send_message(
+        message.chat.id,
+        f"Ваша робота \"{student.work}\". Треба змінити інформацію?",
+        reply_markup=markup,
+    )
     bot.register_next_step_handler(msg, procces_if_change_work, student)
 
 
 def procces_if_change_work(message, student: Student):
     if message.text == constants.YES:
-        msg = bot.send_message(message.chat.id,
-                               f"Введіть місце роботи:")
+        msg = bot.send_message(
+            message.chat.id,
+            f"Введіть місце роботи:",
+        )
         bot.register_next_step_handler(
-            msg, procces_change_work, student=student)
+            msg, procces_change_work, student=student,
+        )
     elif message.text == constants.NO:
         # Here shoud be a check if year_finish is earlier today's date
         # if it is the case, no need to ask update information about university
@@ -90,18 +104,23 @@ def procces_change_work(message, student: Student):
 
 def ask_change_university(message, student: Student):
     markup = build_reply_markup(constants.YES_NO)
-    msg = bot.send_message(message.chat.id,
-                           f"Ваш університет \"{student.university}\". Треба змінити інформацію?",
-                           reply_markup=markup)
+    msg = bot.send_message(
+        message.chat.id,
+        f"Ваш університет \"{student.university}\". Треба змінити інформацію?",
+        reply_markup=markup
+    )
     bot.register_next_step_handler(msg, procces_if_change_university, student)
 
 
 def procces_if_change_university(message, student: Student):
     if message.text == constants.YES:
-        msg = bot.send_message(message.chat.id,
-                               f"Введіть університет:")
+        msg = bot.send_message(
+            message.chat.id,
+            f"Введіть університет:"
+        )
         bot.register_next_step_handler(
-            msg, procces_change_university, student=student)
+            msg, procces_change_university, student=student
+        )
     elif message.text == constants.NO:
         save_info(message, student)
 
