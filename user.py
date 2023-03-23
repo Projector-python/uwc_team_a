a7ac74ce5dc919c5b1aa6b1dc6f55c3fbaafdd3f
Omelchenko Oleksandr <alexandr.omelchenko@gmail.com>
2023-03-23 11:52:42 +0200

    checks

--- user.py
+++ user.py
@@ -13,6 +13,10 @@ def user_panel_processing(message):
             bot.reply_to(
                 message, "Ви вже реєструвались, спробуйте оновити свої дані")
 
+        elif db.is_admin(message.from_user.id):
+            bot.reply_to(
+                message, "Ви зареєстровані як адміністратор")
+
         else:
             student = Student()
             msg = bot.reply_to(
@@ -24,21 +28,35 @@ def user_panel_processing(message):
             student = db.get_student_info(message.from_user.id)
             bot.reply_to(message, student.overview)
 
+        elif db.is_admin(message.from_user.id):
+            bot.reply_to(
+                message, "Ви є адміністратором телеграм бота")
+
         else:
             msg = bot.reply_to(
                 message, constants.REGISTRATION_ASK)
             bot.register_next_step_handler(msg, user_panel_processing)
 
     elif message.text == constants.UPDATE_PROFILE:
-        markup = build_reply_markup(constants.YES_NO)
+        if db.is_user(message.from_user.id):
+            markup = build_reply_markup(constants.YES_NO)
 
-        msg = bot.reply_to(
-            message, f"""Ви хочете оновити свої дані? \n
-            {db.get_student_info(message.from_user.id).overview}""",
-            reply_markup=markup
-        )
-        bot.register_next_step_handler(
-            msg, procces_if_update)
+            msg = bot.reply_to(
+                message, f"""Ви хочете оновити свої дані? \n
+                {db.get_student_info(message.from_user.id).overview}""",
+                reply_markup=markup
+            )
+            bot.register_next_step_handler(
+                msg, procces_if_update)
+
+        elif db.is_admin(message.from_user.id):
+            bot.reply_to(
+                message, "Ви є адміністратором телеграм бота")
+
+        else:
+            msg = bot.reply_to(
+                message, constants.REGISTRATION_ASK)
+            bot.register_next_step_handler(msg, user_panel_processing)
 
     elif message.text == constants.UWC_SMM:
         markup = build_inline_markup(constants.SOCIAL_BUTTONS)
